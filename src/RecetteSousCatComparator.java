f162f006032a9992a54eec017df05371e673df72
HarnoRanaivo <razanajato@etu.unistra.fr>
2013-04-30 16:30:36 +0200

    remaniements

--- src/RecetteSousCatComparator.java
+++ src/RecetteSousCatComparator.java
@@ -18,35 +18,4 @@ public class RecetteSousCatComparator implements Comparator<Recette>
     {
         return r1.getSousCategorie().compareTo(r2.getSousCategorie());
     }
-
-    // /**
-    //  * Teste d'égalité de deux recettes
-    //  *
-    //  * @param o
-    //  *      Recette à comparer.
-    //  * @return true si elles ont égales, false sinon.
-    //  */
-    // public boolean equals(Object o)
-    // {
-    //     if (o instanceof Recette)
-    //     {
-    //         Recette r = (Recette) o;
-    //         if (this.nom.equals(r.nom)
-    //             && this.livre.equals(r.livre)
-    //             && this.page == r.page
-    //             && this.texte.equals(r.texte)
-    //             && this.note == r.note
-    //             && this.commentaire.equals(r.commentaire)
-    //             && this.categorie.equals(r.categorie)
-    //             && this.sousCategorie.equals(r.sousCategorie)
-    //             && this.date.equals(r.date)
-    //             && this.lien.equals(r.lien)
-    //             )
-    //             return true;
-    //         else
-    //             return false;
-    //     }
-    //     else
-    //         return false;
-    // }
 }
